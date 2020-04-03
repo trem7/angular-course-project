9b9ee7a423001189f728ee65edbb85779668b806
Adam <adam.tremblett7@gmail.com>
2020-04-03 13:38:18 -0400

    Submitting the recipe edit form

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -21,6 +21,12 @@ export class RecipeListComponent implements OnInit {
   }
 
   ngOnInit(): void {
+    this.recipeService.recipesChanged
+      .subscribe(
+        (recipes: Recipe[]) => {
+          this.recipes = recipes;
+        }
+      );
     this.recipes = this.recipeService.getRecipes();
   }
 

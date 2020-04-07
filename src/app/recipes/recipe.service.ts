b4926ec89ed09de8c7c8bf5d64aed5fd8c85451b
Adam <adam.tremblett7@gmail.com>
2020-04-07 10:30:52 -0400

    Transforming response data

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -9,25 +9,27 @@ import { Subject } from "rxjs";
 export class RecipeService {
   recipesChanged = new Subject<Recipe[]>();
 
-  private recipes: Recipe[] = [
-    new Recipe(
-      'Tasty Schnitzel',
-      'A super tasty Schnitzel',
-      'https://upload.wikimedia.org/wikipedia/commons/4/46/Pork_schnitzel.jpg',
-      [
-        new Ingredient('Meat', 1),
-        new Ingredient('French Fries', 20)
-      ]),
-    new Recipe(
-      'A pizza',
-      'A regular old pizza',
-      'https://torange.biz/photo/38/IMAGE/pizza-health-recipe-38030.jpg',
-      [
-        new Ingredient('Crust', 1),
-        new Ingredient('Sauce', 1),
-        new Ingredient('Cheese', 1)
-      ])
-  ];
+  // private recipes: Recipe[] = [
+  //   new Recipe(
+  //     'Tasty Schnitzel',
+  //     'A super tasty Schnitzel',
+  //     'https://upload.wikimedia.org/wikipedia/commons/4/46/Pork_schnitzel.jpg',
+  //     [
+  //       new Ingredient('Meat', 1),
+  //       new Ingredient('French Fries', 20)
+  //     ]),
+  //   new Recipe(
+  //     'A pizza',
+  //     'A regular old pizza',
+  //     'https://torange.biz/photo/38/IMAGE/pizza-health-recipe-38030.jpg',
+  //     [
+  //       new Ingredient('Crust', 1),
+  //       new Ingredient('Sauce', 1),
+  //       new Ingredient('Cheese', 1)
+  //     ])
+  // ];
+
+  private recipes: Recipe[] = [];
 
   constructor(private slService: ShoppingListService) {}
 
--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from "@angular/core";
-import { HttpClient } from '@angular/common/http'
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
 
 import { RecipeService } from "../recipes/recipe.service";
 import { Recipe } from "../recipes/recipe.model";
@@ -22,9 +23,15 @@ export class DataStorageService {
 
   fetchRecipes() {
     this.http
-      .get<Recipe[]>(
-        'https://ng-course-recipe-book-a3565.firebaseio.com/recipes.json'
-      )
+      .get<Recipe[]>('https://ng-course-recipe-book-a3565.firebaseio.com/recipes.json')
+      .pipe(map(recipes => {
+        return recipes.map(recipe => {
+          return {
+            ...recipe,
+            ingredients: recipe.ingredients ? recipe.ingredients : []
+          };
+        });
+      }))
       .subscribe(recipes => {
         this.recipeService.setRecipes(recipes);
       })

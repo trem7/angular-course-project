82d17956c52e5d5ee97ce7ff4849c28c0b4e63b1
Adam <adam.tremblett7@gmail.com>
2020-04-08 10:21:32 -0400

    Reflecting the auth state in the ui

--- src/app/auth/user.model.ts
+++ src/app/auth/user.model.ts
@@ -4,12 +4,12 @@ export class User {
     public id: string,
     private _token: string,
     private _tokenExpirationDate: Date
-    ) {}
+  ) {}
 
-    get token() {
-      if (!this._tokenExpirationDate || new Date() > this._tokenExpirationDate) {
-        return null;
-      }
-      return this._token;
+  get token() {
+    if (!this._tokenExpirationDate || new Date() > this._tokenExpirationDate) {
+      return null;
     }
+    return this._token;
+  }
 }

3510da47443bdc0e8cbf0a39a3a1173756f4de74
Adam <adam.tremblett7@gmail.com>
2020-04-08 10:56:48 -0400

    Adding the token to outgoing data

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,13 +1,14 @@
 import { Injectable } from "@angular/core";
-import { HttpClient } from '@angular/common/http';
-import { map, tap } from 'rxjs/operators';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { map, tap, take, exhaustMap } from 'rxjs/operators';
 
 import { RecipeService } from "../recipes/recipe.service";
 import { Recipe } from "../recipes/recipe.model";
+import { AuthService } from "../auth/auth.service";
 
 @Injectable({providedIn: 'root'})
 export class DataStorageService {
-  constructor(private http: HttpClient, private recipeService: RecipeService) {}
+  constructor(private http: HttpClient, private recipeService: RecipeService, private authService: AuthService) {}
 
   storeRecipes() {
     const recipes = this.recipeService.getRecipes();
@@ -22,9 +23,16 @@ export class DataStorageService {
   }
 
   fetchRecipes() {
-    return this.http
-      .get<Recipe[]>('https://ng-course-recipe-book-a3565.firebaseio.com/recipes.json')
-      .pipe(map(recipes => {
+    return this.authService.user.pipe(take(1), exhaustMap(user => {
+      return this.http
+      .get<Recipe[]>(
+        'https://ng-course-recipe-book-a3565.firebaseio.com/recipes.json',
+        {
+          params: new HttpParams().set('auth', user.token)
+        }
+      );
+    }),
+      map(recipes => {
         return recipes.map(recipe => {
           return {
             ...recipe,
@@ -35,6 +43,6 @@ export class DataStorageService {
       tap(recipes => {
         this.recipeService.setRecipes(recipes);
       })
-    )
+    );
   }
 }
